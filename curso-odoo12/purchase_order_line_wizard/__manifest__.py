8a5654a6ef52fb826fd41cbaa94aaae7b7a645d9
artur <artur@gmail.com>
2019-09-24 17:02:11 +0200

    Añadido listar pedidos compra

--- curso-odoo12/purchase_order_line_wizard/__manifest__.py
+++ curso-odoo12/purchase_order_line_wizard/__manifest__.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Product Extend",
+    'name': "Purchase Order Line Wizard",
 
     'summary': """
-        Library Management
+        Wizard Purchase Order Line
     """,
 
-    'author': "Aloha",
+    'author': "Aloh",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['product'],
+    'depends': ['purchase'],
 
     # always loaded
     'data': [
         # 'views/views.xml',
-         'views/product_view.xml'
+        # 'views/product_view.xml'
+        'views/purchase_order_line_wizard_view.xml'
     ],
     'installable': True,
     'application': True,
